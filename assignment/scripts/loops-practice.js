0a0cf38b07cbcc1380396346f76613ff9cc63f67
Ismail Ali <ishaliprime@gmail.com>
2023-04-16 13:28:37 -0500

    completed problems 1d and all of 2

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -29,11 +29,14 @@ for( let i=3; i<6; i++){
 console.log('count even numbers from 2 to 10 (2, 4, 6, 8, 10):');
 for( let i=2; i<11; i+=2){
   console.log(i);
-}
+}// i changed the value of i to equal 2 but less than 11 and incremented i by 2
 
 // 1.d. (STRETCH) TODO: Write a for loop to do a countdown from 5 to 0
 console.log('STRETCH: countdown from 5 to 0');
-for( let i=5; i>=0)
+for( let i=5; i>=0; i--){
+  console.log(i);
+}// i had to change the starting value of i to 5 but greater than or equal to 0
+// so that 0 was included in the loop. Then I had to decrement i by 1 each time.
 
 // 2. For of loops  
 console.log('---- 2. For Of loop ----');
@@ -41,6 +44,9 @@ let stars = ['Polaris', 'Gacrux', 'Formalhaut', 'Rigel', 'Deneb']
 
 // 2.a. TODO: Write a for of loop to console.log each star in the 'stars' array
 console.log('Some stars:');
+for( names of stars){
+  console.log('for of loop:', names);
+}
 
 
 // 3. While loops  
@@ -48,6 +54,7 @@ console.log('---- 3. While loop ----');
 
 // 3.a. TODO: Write a while loop to console.log each star in the 'stars' array
 console.log('Some stars using while:');
+
 
 // 3.b. TODO: Write a while loop to console.log the numbers from 0 to 5
 console.log('count from 0 to 5');
